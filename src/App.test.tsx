2f4d0d942d838183a6cd35edb6cae58f442824d0
Ilia Cimpoes <10390772+ICimpoes@users.noreply.github.com>
2024-01-03 11:33:25 +0200

    Setup svg canvas with palette and workspace (#2)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,44 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import {
+    getAllByTestId,
+    getByTestId,
+    render,
+    screen,
+} from "@testing-library/react";
 import App from "./App";
 
-test("renders hello world text", () => {
-    render(<App />);
-    const helloElement = screen.getByText(/hello world!/i);
-    expect(helloElement).toBeInTheDocument();
+describe("<App />", () => {
+    beforeEach(() => {
+        render(<App />);
+    });
+
+    describe("#render", () => {
+        it("has editor", () => {
+            const editor = screen.getByTestId("editor");
+
+            expect(editor).toBeDefined();
+        });
+    });
+
+    describe("editor", () => {
+        const editor = (): HTMLElement => {
+            return screen.getByTestId("editor");
+        };
+
+        it("has palette", () => {
+            const palette = getByTestId(editor(), "palette");
+
+            expect(palette).toBeDefined();
+
+            const shapes = getAllByTestId(palette, "shape");
+
+            expect(shapes).toHaveLength(3);
+        });
+
+        it("has workspace", () => {
+            const workspace = getAllByTestId(editor(), "workspace");
+
+            expect(workspace).toHaveLength(1);
+        });
+    });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,53 @@
-import React from "react";
-import logo from "./logo.svg";
+import * as React from "react";
 import "./App.css";
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>hello world!</p>
-            </header>
-        </div>
-    );
-}
+export default class App extends React.Component {
+    public render(): JSX.Element {
+        return (
+            <div data-testid="editor" className="editor">
+                <div data-testid="palette" className="palette">
+                    <div data-testid="shape" className="shape">
+                        <svg
+                            width="50"
+                            height="50"
+                            version="1.1"
+                            xmlns="http://www.w3.org/2000/svg"
+                            xmlnsXlink="http://www.w3.org/1999/xlink"
+                        >
+                            <use xlinkHref="assets/shapes.svg#square" />
+                        </svg>
+                    </div>
+                    <div data-testid="shape" className="shape">
+                        <svg
+                            width="50"
+                            height="50"
+                            version="1.1"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <use xlinkHref="assets/shapes.svg#circle" />
+                        </svg>
+                    </div>
+                    <div data-testid="shape" className="shape">
+                        <svg
+                            width="50"
+                            height="50"
+                            version="1.1"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <use xlinkHref="assets/shapes.svg#triangle" />
+                        </svg>
+                    </div>
+                </div>
 
-export default App;
+                <div data-testid="workspace" className="workspace">
+                    <svg
+                        className="canvas"
+                        viewBox="0 0 1200 600"
+                        version="1.1"
+                        xmlns="http://www.w3.org/2000/svg"
+                    ></svg>
+                </div>
+            </div>
+        );
+    }
+}
